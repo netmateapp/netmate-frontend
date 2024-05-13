98398a6038dc8e0d31f8faaf497acfe95e809e2b
amata1219 <suzukiamata@gmail.com>
2024-05-14 00:45:27 +0900

    追加: タブフォーカス限定CSS適用

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
-    import "../app.css";
+  import { browser } from "$app/environment";
+  
+  import "../app.css";
+  import "../i18n.svelte";
+
+  // a11y: タブ時のみフォーカスが表示されるようにする
+  if (browser) {
+    document.body.addEventListener("mousedown", () => {
+      document.body.classList.add("using-mouse");
+      console.log("layout.s using-mouse");
+    });
+
+    document.body.addEventListener("keydown", event => {
+      console.log(event.key);
+      if (event.key === "Tab") {
+        document.body.classList.remove("using-mouse");
+        console.log("layout.s using-tab: " + document.body.classList);
+      }
+    });
+  }
 </script>
 
 <slot />
