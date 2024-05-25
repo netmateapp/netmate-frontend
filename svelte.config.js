23293005b02a49899ae19e65dd6881d5012a10d3
amata1219 <suzukiamata@gmail.com>
2024-05-26 00:16:56 +0900

    修正: コンパイラプションを適切な位置に修正

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
-import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-    vitePreprocess(),
-    svelte({
-      compilerOptions: {
-        customElement: true
-      }
-    })
-  ],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+  
+  compilerOptions: {
+    customElement: true,
+  }
 };
 
 export default config;
