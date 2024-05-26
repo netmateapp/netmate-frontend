0fe9fe043ec059f17622d57d347456ed377d13e9
amata1219 <suzukiamata@gmail.com>
2024-05-26 17:01:21 +0900

    修正: HMRをフルリロードに

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,31 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+function isWebComponentSvelte(code) {
+  const svelteOptionsIdx = code.indexOf('<svelte:options ')
+  if(svelteOptionsIdx < 0) {
+      return false
+  }
+  const tagOptionIdx = code.indexOf('tag:', svelteOptionsIdx)
+  const svelteOptionsEndIdx = code.indexOf('>',svelteOptionsIdx);
+  return tagOptionIdx > svelteOptionsIdx && tagOptionIdx < svelteOptionsEndIdx;
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+  vitePlugin: {
+    dynamicCompileOptions({code}) {
+      if (isWebComponentSvelte(code)) {
+          return {
+              customElement: true,
+          }
+      }
+    }
+  },
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-  
-  compilerOptions: {
-    customElement: true,
-  }
 };
 
 export default config;
---
+++ vite-plugins/reloadSettings.ts
@@ -0,0 +1,15 @@
+import { PluginOption } from 'vite';
+
+export default function reloadSettings(): PluginOption {
+  return {
+    name: 'sync-reload',
+    enforce: 'pre',
+    apply: 'serve',
+    handleHotUpdate({ server }) {
+      server.ws.send({
+        type: 'full-reload',
+      });
+      return [];
+    }
+  };
+}
