28c432923920c42704c023b61c3a3c015f0e9565
amata1219 <suzukiamata@gmail.com>
2024-05-31 00:19:56 +0900

    整理: エラー型を簡素化

--- src/lib/uuid.ts
+++ src/lib/uuid.ts
@@ -1,16 +1,14 @@
-import { Err, Ok, type Result } from "./result";
+import { err, ok, type Result } from "./result";
 
-export const enum UuidError {
-  INVALID_FORMAT
-}
+type InvalidFormatError = {};
 
 const UUID_V4_REGEX = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
 export class Uuid4 {
   constructor(private hexadecimalRepresentation: string) {}
 
-  static of(hexadecimalRepresentation: string): Result<UuidError, Uuid4> {
-    return UUID_V4_REGEX.test(hexadecimalRepresentation) ? new Ok(new Uuid4(hexadecimalRepresentation)) : new Err(UuidError.INVALID_FORMAT);
+  static of(hexadecimalRepresentation: string): Result<InvalidFormatError, Uuid4> {
+    return UUID_V4_REGEX.test(hexadecimalRepresentation) ? ok(new Uuid4(hexadecimalRepresentation)) : err({});
   }
 
   asHexadecimalRepresentation(): string {
@@ -23,8 +21,8 @@ const UUID_V7_REGEX = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-7[0-9a-fA-F]{3}-[89abAB][0
 export class Uuid7 {
   constructor(private hexadecimalRepresentation: string) {}
 
-  static of(hexadecimalRepresentation: string): Result<UuidError, Uuid7> {
-    return UUID_V7_REGEX.test(hexadecimalRepresentation) ? new Ok(new Uuid7(hexadecimalRepresentation)) : new Err(UuidError.INVALID_FORMAT);
+  static of(hexadecimalRepresentation: string): Result<InvalidFormatError, Uuid7> {
+    return UUID_V7_REGEX.test(hexadecimalRepresentation) ? ok(new Uuid7(hexadecimalRepresentation)) : err({});
   }
 
   asHexadecimalRepresentation(): string {
