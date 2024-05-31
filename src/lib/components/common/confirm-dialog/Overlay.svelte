58c7181d93b08fb003a0d2ee9ff26c71f57b47d0
amata1219 <suzukiamata@gmail.com>
2024-05-31 18:14:52 +0900

    追加: オーバーレイ

---
+++ src/lib/components/common/confirm-dialog/Overlay.svelte
@@ -0,0 +1,13 @@
+<div class="overlay"></div>
+
+<style>
+  .overlay {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    z-index: 1;
+  }
+</style>
