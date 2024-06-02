4e6e629ecc9351ac38cde6b4911f4fe71f53194b
amata1219 <suzukiamata@gmail.com>
2024-06-02 18:53:19 +0900

    追加: データベースヘッダー

---
+++ src/lib/components/database/Header.svelte
@@ -0,0 +1,76 @@
+<script lang="ts" context="module">
+  type Sort = "history" | "newest" | "oldest";
+</script>
+
+<script lang="ts">
+  import { createTranslator } from "$lib/i18n.svelte";
+  import { makeKeydownHandler } from "$lib/utils.svelte";
+
+  const _ = createTranslator("database", "share-list");
+
+  let { resultCount, initialSort }: { resultCount: number; initialSort: Sort } =
+    $props();
+
+  const SORTS: Sort[] = ["history", "newest", "oldest"];
+  let selectedSort: Sort = $state(initialSort);
+
+  function isSelected(sort: Sort) {
+    return sort === selectedSort;
+  }
+
+  function handleInteractToButton(sort: Sort) {
+    if (!isSelected(sort)) selectedSort = sort;
+  }
+</script>
+
+<div class="header">
+  <span class="total-hits">{_("total-hits", { count: resultCount })}</span>
+  <div class="buttons">
+    {#each SORTS as sort}
+      <button
+        class="button"
+        class:selected={isSelected(sort)}
+        onclick={() => handleInteractToButton(sort)}
+        onkeydown={makeKeydownHandler(() => handleInteractToButton(sort))}
+      >
+        <span class="label">{_(sort)}</span>
+      </button>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+  }
+
+  .total-hits {
+    color: var(--dark-gray);
+    font-family: var(--primary-font);
+    font-size: 0.875rem;
+  }
+
+  .buttons {
+    display: flex;
+    align-items: flex-start;
+    gap: 0.5rem;
+  }
+
+  .button {
+    display: flex;
+    height: 2.25rem;
+    padding: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .label {
+    color: var(--dark-gray);
+    font-family: var(--primary-font);
+    font-size: 0.875rem;
+  }
+</style>
