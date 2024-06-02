b8f5e495dd852f2054623c6d9f1160f0ccb07f8d
amata1219 <suzukiamata@gmail.com>
2024-06-02 18:29:55 +0900

    追加: スペースの地名コンポーネント

---
+++ src/lib/components/space/Location.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let { locationName }: { locationName: string } = $props();
+</script>
+
+<span class="location-name">{locationName}</span>
+
+<style>
+  .location-name {
+    color: var(--accent-color);
+    text-align: center;
+    font-family: var(--primary-font);
+    font-size: 2.25rem;
+    line-height: 2.625rem
+  }
+</style>
