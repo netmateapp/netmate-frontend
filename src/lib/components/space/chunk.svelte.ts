eb4d7df961a0eed05196422f66d77e0a3a9bfce2
amata1219 <suzukiamata@gmail.com>
2024-06-08 00:36:20 +0900

    改修: 責任分界点を明確化

--- src/lib/components/space/chunk.svelte.ts
+++ src/lib/components/space/chunk.svelte.ts
@@ -1,147 +1,113 @@
-import type { Position } from "./move.svelte";
-
 const CHUNK_SCALE = 10;
 
-class ChunkKey {
-  constructor(public readonly key: string) {}
-}
-
-class ChunkIndex {
-  constructor(public readonly index: number) {}
-}
-
-class ChunkLocation {
-  constructor(
-    public readonly chunkX: number,
-    public readonly chunkY: number
-  ) {}
-}
-
-type Fetcher = (chunkIndexes: ChunkIndex[]) => Chunk[];
+export type ChunksFetcher = (requiredChunkIndexes: Set<number>) => Chunk[];
+export type ChunksContentsRenderer = (currentPositionX: number, currentPositionY: number, loader: ChunkLoader) => void;
 
 const PREFETCH_RADIUS = 1;
 const DIRECTIONAL_PREFETCH_RADIUS = 5;
 
-export class ChunkSystem {
-  private readonly keysToChunks = new Map<ChunkKey, Chunk>();
-  private previousChunkX = 0;
-  private previousChunkY = 0;
+export class DynamicChunkLoader {
+  private previousChunkX: number = 0;
+  private previousChunkY: number = 0;
+
+  constructor(
+    private readonly chunkLoader: ChunkLoader,
+    private readonly chunksContentsRenderer: ChunksContentsRenderer
+  ) {}
+
+  initialLoad(initialPositionX: number, initialPositionY: number) {
+    const chunkX = toChunkX(initialPositionX);
+    const chunkY = toChunkY(initialPositionY);
+    this.chunkLoader.loadChunksAround(chunkX, chunkY);
+    this.previousChunkX = chunkX;
+    this.previousChunkY = chunkY;
+    this.chunksContentsRenderer(initialPositionX, initialPositionY, this.chunkLoader);
+  }
+
+  onPositionUpdate(x: number, y: number) {
+    const chunkX = toChunkX(x);
+    const chunkY = toChunkY(y);
+    if (this.isCrossChunks(chunkX, chunkY)) {
+      this.chunkLoader.loadChunksDirectionally(chunkX, chunkY, this.previousChunkX, this.previousChunkY);
+      this.previousChunkX = chunkX;
+      this.previousChunkY = chunkY;
+      this.chunksContentsRenderer(x, y, this.chunkLoader);
+    }
+  }
+
+  private isCrossChunks(chunkX: number, chunkY: number): boolean {
+    return chunkX !== this.previousChunkX || chunkY !== this.previousChunkY;
+  }
+}
+
+export class ChunkLoader {
+  private readonly keysToChunks = new Map<string, Chunk>();
+
   private visibleChunks: Chunk[] = $state([]);
 
   constructor(
-    private readonly fetcher: Fetcher,
-    private readonly position: Position
+    private readonly chunksFetcher: ChunksFetcher,
   ) {}
 
-  loadInitialChunks(initialX: number, initialY: number) {
-    const chunkX = toChunkX(initialX);
-    const chunkY = toChunkY(initialY);
-
-    const chunkIndexesToLoad: number[] = [];
-    const chunkIndexesToKeys = new Map<number, string>();
-
-    // 周囲のチャンクを読み込み
-    for (var cx = chunkX - PREFETCH_RADIUS; cx <= chunkX + PREFETCH_RADIUS; cx++) {
-      for (var cy = chunkY - PREFETCH_RADIUS; cy <= chunkY + PREFETCH_RADIUS; cy++) {
-        const key = toChunkKey(cx, cy);
-        if (!this.keysToChunks.has(key)) {
-          const index = toChunkIndex(cx, cy);
-          chunkIndexesToKeys.set(index, key);
-          chunkIndexesToLoad.push(index);
-        }
+  private calculateRequiredChunkIndexes(chunkX: number, chunkY: number, radius: number): number[] {
+    const requiredChunkIndexes: number[] = [];
+    for (var cx = chunkX - radius; cx <= chunkX + radius; cx++) {
+      for (var cy = chunkY - radius; cy <= chunkY + radius; cy++) {
+        if (!this.isChunkLoaded(cx, cy)) requiredChunkIndexes.push(toChunkIndex(cx, cy));
       }
     }
+    return requiredChunkIndexes;
+  }
 
-    const fetchedChunks: Map<number, Chunk> = fetchChunks(this.type, this.id, chunkIndexesToLoad);
-    for (var [index, chunk] of fetchedChunks.entries()) {
-      const key = chunkIndexesToKeys.get(index);
+  private isChunkLoaded(chunkX: number, chunkY: number): boolean {
+    return this.keysToChunks.has(toChunkKey(chunkX, chunkY));
+  }
+
+  private loadChunks(chunks: Chunk[]) {
+    for (var chunk of chunks) {
+      const key = toChunkKey(chunk.chunkX, chunk.chunkY);
       if (key) this.keysToChunks.set(key, chunk);
     }
-
-    //test
-
-    for (var x = -1; x < 2; x++) {
-      for (var y = -1; y < 2; y++) {
-        const chX = toChunkX(x * 1024);
-        const chY = toChunkY(y * 1024);
-        const key = toChunkKey(chX, chY);
-        this.keysToChunks.set(key, new SharesChunk(chX, chY));
-      }
-    }
-
-    const newVisibleChunks: Chunk[] = [];
-    for (var cax = chunkX - PREFETCH_RADIUS; cax <= chunkX + PREFETCH_RADIUS; cax++) {
-      for (var cay = chunkY - PREFETCH_RADIUS; cay <= chunkY + PREFETCH_RADIUS; cay++) {
-        const key = toChunkKey(cax, cay);
-        console.log(key);
-        const chunk = this.keysToChunks.get(key);
-        if (chunk) newVisibleChunks.push(chunk);
-      }
-    }
-
-    this.visibleChunks = newVisibleChunks;
-    console.log(this.visibleChunks.length);
   }
 
-  onMove() {
-    // debounce必要?
-    const chunkX = toChunkX(getCurrentX());
-    const chunkY = toChunkY(getCurrentY());
-    if (chunkX !== this.previousChunkX || chunkY !== this.previousChunkY) {
-      this.onCrossChunks(chunkX, chunkY);
-
-      const newVisibleChunks: Chunk[] = [];
-      for (var cx = chunkX - PREFETCH_RADIUS; cx <= chunkX + PREFETCH_RADIUS; cx++) {
-        for (var cy = chunkY - PREFETCH_RADIUS; cy <= chunkY + PREFETCH_RADIUS; cy++) {
-          const key = toChunkKey(cx, cy);
-          const chunk = this.keysToChunks.get(key);
-          if (chunk) newVisibleChunks.push(chunk);
-        }
+  pchunksAround(x: number, y: number, radius: number): Chunk[] {
+    const chunkX = toChunkX(x);
+    const chunkY = toChunkY(y);
+    const chunks: Chunk[] = [];
+    for (var cx = chunkX - radius; cx <= chunkX + radius; cx++) {
+      for (var cy = chunkY - radius; cy <= chunkY + radius; cy++) {
+        const chunk = this.keysToChunks.get(toChunkKey(cx, cy));
+        if (chunk) chunks.push(chunk);
       }
-
-      this.visibleChunks = newVisibleChunks;
     }
+    return chunks;
   }
 
-  private onCrossChunks(chunkX: number, chunkY: number) {
-    const chunkIndexesToLoad: number[] = [];
-    const chunkIndexesToKeys = new Map<number, string>();
+  loadChunksAround(chunkX: number, chunkY: number) {
+    this.loadChunksDirectionally(chunkX, chunkY, chunkX, chunkY);
+  }
+
+  loadChunksDirectionally(chunkX: number, chunkY: number, previousChunkX: number, previousChunkY: number) {
+    const requiredChunkIndexes = new Set<number>();
 
     // 周囲のチャンクを読み込み
-    for (var chx = chunkX - PREFETCH_RADIUS; chx <= chunkX + PREFETCH_RADIUS; chx++) {
-      for (var chy = chunkY - PREFETCH_RADIUS; chy <= chunkY + PREFETCH_RADIUS; chy++) {
-        const key = toChunkKey(chx, chy);
-        if (!this.keysToChunks.has(key)) {
-          const index = toChunkIndex(chx, chy);
-          chunkIndexesToKeys.set(index, key);
-          chunkIndexesToLoad.push(index);
-        }
-      }
-    }
+    this.calculateRequiredChunkIndexes(chunkX, chunkY, PREFETCH_RADIUS)
+      .forEach(i => requiredChunkIndexes.add(i));
 
     // 移動方向のチャンクを読み込み
-    const dx = chunkX - this.previousChunkX;
-    const dy = chunkY - this.previousChunkY;
-    for (let i = 1; i <= this.directionalPrefetchRedius; i++) {
-      const baseChunkX = chunkX + i * Math.sign(dx);
-      const baseChunkY = chunkY + i * Math.sign(dy);
-      for (var cx = baseChunkX - PREFETCH_RADIUS; cx <= baseChunkX + PREFETCH_RADIUS; cx++) {
-        for (var cy = baseChunkY - PREFETCH_RADIUS; cy <= baseChunkY + PREFETCH_RADIUS; cy++) {
-          const key = toChunkKey(cx, cy);
-          if (!this.keysToChunks.has(key)) {
-            const index = toChunkIndex(cx, cy);
-            chunkIndexesToKeys.set(index, key);
-            chunkIndexesToLoad.push(index);
-          }
-        }
+    const dx = chunkX - previousChunkX;
+    const dy = chunkY - previousChunkY;
+    if (dx !== 0 || dy !== 0) {
+      for (let i = 1; i <= DIRECTIONAL_PREFETCH_RADIUS; i++) {
+        const baseChunkX = chunkX + i * Math.sign(dx);
+        const baseChunkY = chunkY + i * Math.sign(dy);
+        this.calculateRequiredChunkIndexes(baseChunkX, baseChunkY, PREFETCH_RADIUS)
+          .forEach(i => requiredChunkIndexes.add(i));
       }
     }
 
-    const fetchedChunks: Map<number, Chunk> = fetchChunks(this.type, this.id, chunkIndexesToLoad);
-    for (var [index, chunk] of fetchedChunks.entries()) {
-      const key = chunkIndexesToKeys.get(index);
-      if (key) this.keysToChunks.set(key, chunk);
-    }
+    const requiredChunks = this.chunksFetcher(requiredChunkIndexes);
+    this.loadChunks(requiredChunks);
   }
 }
 
@@ -174,7 +140,6 @@ export class SpaceCoreChunk implements Chunk {
   }
 }
 
-
 function toChunkX(x: number): number {
   return x >> CHUNK_SCALE;
 }
@@ -187,7 +152,7 @@ function toChunkKey(chunkX: number, chunkY: number): string {
   return `${chunkX}${chunkY}`;
 }
 
-function toChunkIndex(chunkX: number, chunkY: number): ChunkIndex {
+function toChunkIndex(chunkX: number, chunkY: number): number {
   let distanceFromOrigin = Math.max(Math.abs(chunkX), Math.abs(chunkY));
 
   // chunkGroupは短辺が1のチャンク列の番号、offsetはチャンク列内での位置
@@ -217,32 +182,31 @@ function toChunkIndex(chunkX: number, chunkY: number): ChunkIndex {
   let chunkGroupStartIndex = chunkGroupLongSideLength * chunkGroupLongSideLength + (chunkGroupId % 2 === 0 ? -chunkGroupLongSideLength : 0);
 
   // チャンクグループ内での位置と足し合わせ、全体におけるチャンクの番号を返す
-  return new ChunkIndex(chunkGroupStartIndex + offset);
+  return chunkGroupStartIndex + offset;
 }
 
-function toChunkLocation(index: ChunkIndex): ChunkLocation {
-  let n = index.index;
-
-  if (n === 0) return new ChunkLocation(0, 0); // 原点の場合
+export function toChunkXY(index: number): [number, number] {
+  // 原点の場合
+  if (index === 0) return [0, 0];
 
   // 螺旋の層を決定する
-  let layer = Math.floor((Math.sqrt(n) + 1) / 2);
+  let layer = Math.floor((Math.sqrt(index) + 1) / 2);
 
   // この層の開始番号を計算
   let layerStart = Math.pow(2 * layer - 1, 2);
   
   // 層内の位置を計算
-  let position = n - layerStart;
+  let position = index - layerStart;
   let sideLength = 2 * layer;
 
   // それぞれの辺の位置を計算
   if (position < sideLength) { // 右辺
-      return new ChunkLocation(layer, (layer - 1) - position);
+      return [layer, (layer - 1) - position];
   }if (position < 2 * sideLength) { // 下辺
-      return new ChunkLocation((layer - 1) - (position - sideLength), -layer);
+      return [(layer - 1) - (position - sideLength), -layer];
   } else if (position < 3 * sideLength) { // 左辺
-      return new ChunkLocation(-layer, -(layer - 1) + (position - sideLength * 2));
+      return [-layer, -(layer - 1) + (position - sideLength * 2)];
   } else { // 上辺
-      return new ChunkLocation(-(layer - 1) + (position - sideLength * 3), layer);
+      return [-(layer - 1) + (position - sideLength * 3), layer];
   }
 }
