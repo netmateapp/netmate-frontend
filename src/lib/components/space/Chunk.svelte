c012e5bbf8485e7cc839d1859c0bc23a998382e5
amata1219 <suzukiamata@gmail.com>
2024-06-08 23:47:41 +0900

    追加: チャンクコンポーネント

---
+++ src/lib/components/space/Chunk.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let { apparentX, apparentY }: { apparentX: number, apparentY: number } = $props();
+</script>
+
+<div class="chunk" style="bottom: {apparentY}px; left: {apparentX}px;">
+  <slot></slot>
+</div>
+
+<style>
+  .chunk {
+    position: fixed;
+    width: 64rem;
+    height: 64rem;
+  }
+</style>
