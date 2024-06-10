f9ab93e01d265371baf92d1df91842bec53c9843
amata1219 <suzukiamata@gmail.com>
2024-06-10 11:12:58 +0900

    追加: テスト用の簡易UUID生成関数

--- src/lib/uuid.ts
+++ src/lib/uuid.ts
@@ -1,4 +1,4 @@
-import { err, ok, type Result } from "./result";
+import { err, Ok, ok, type Result } from "./result";
 
 const UUID_V4_REGEX = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
@@ -14,6 +14,24 @@ export class Uuid4 {
   }
 }
 
+export function genTestUuid4(): Uuid4 {
+  let uuid = '';
+  for (let i = 0; i < 32; i++) {
+      const random = Math.random() * 16 | 0;
+      if (i == 8 || i == 12 || i == 16 || i == 20) {
+          uuid += '-';
+      }
+      if (i == 12) {
+          uuid += '4';
+      } else if (i == 16) {
+          uuid += (random & 0x3 | 0x8).toString(16);
+      } else {
+          uuid += random.toString(16);
+      }
+  }
+  return (Uuid4.from(uuid) as Ok<{}, Uuid4>).value;
+}
+
 const UUID_V7_REGEX = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-7[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
 export class Uuid7 {
@@ -26,4 +44,11 @@ export class Uuid7 {
   asHexadecimalRepresentation(): string {
     return this.hexadecimalRepresentation;
   }
+}
+
+export function genTestUuid7(): Uuid7 {
+  let uuid4: string[] = genTestUuid4().asHexadecimalRepresentation().split("");
+  uuid4[14] = "7";
+  let uuid7: string = uuid4.reduce((acc, c) => acc + c);
+  return (Uuid7.from(uuid7) as Ok<{}, Uuid7>).value;
 }
