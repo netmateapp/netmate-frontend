c1f3d8fd7bfb8f9984e7abc8fa097af2b4288933
amata1219 <suzukiamata@gmail.com>
2024-06-11 21:12:15 +0900

    追加: 一齧り共有の値オブジェクト

---
+++ src/routes/tags/[id=uuidv4]/space/domain/shareNibble.ts
@@ -0,0 +1,61 @@
+import { calculateCharactersCosts } from "$lib/cjk.svelte";
+import type { Option } from "$lib/option";
+import type { HandleId } from "./handle";
+import { type ShareId, type Timestamp, type ConversationsCount, type Title, type MediaId, NetmateImageId } from "./share";
+
+const MAX_LEAD_SENTENCES_CHARACTERS_COST = 1218;
+
+export class LeadSenetences {
+  public readonly sentences: string;
+
+  constructor(sentences: string) {
+    if (!LeadSenetences.isValid(sentences)) throw new Error(`A character cost of sentences must be less than or equal to ${MAX_LEAD_SENTENCES_CHARACTERS_COST}.`);
+    this.sentences = sentences;
+  }
+
+  private static isValid(sentences: string): boolean {
+    return calculateCharactersCosts(sentences) <= MAX_LEAD_SENTENCES_CHARACTERS_COST;
+  }
+}
+
+export class ShareNibble {
+  public readonly id: ShareId;
+  public readonly sharerId: HandleId;
+  public readonly timestamp: Timestamp;
+  public readonly conversationsCount: ConversationsCount;
+  public readonly title: Option<Title>;
+  public readonly leadSentences: Option<LeadSenetences>;
+  public readonly thumbnailMediaId: Option<MediaId>;
+  public readonly shouldProcessThumbnailImage: boolean;
+
+  constructor(
+    id: ShareId,
+    sharerId: HandleId,
+    timestamp: Timestamp,
+    conversationsCount: ConversationsCount,
+    title?: Option<Title>,
+    leadSentences?: Option<LeadSenetences>,
+    thumbnailMediaId?: Option<MediaId>,
+    shouldProcessThumbnailImage: boolean = false,
+  ) {
+    if (!ShareNibble.isValid(thumbnailMediaId, shouldProcessThumbnailImage)) throw new Error(`A shouldProcessThumbnailImage cannot be set to true unless a thumbnail media is an image.`);
+
+    this.id = id;
+    this.sharerId = sharerId;
+    this.timestamp = timestamp;
+    this.conversationsCount = conversationsCount;
+    this.title = title;
+    this.leadSentences = leadSentences;
+    this.thumbnailMediaId = thumbnailMediaId;
+    this.shouldProcessThumbnailImage = shouldProcessThumbnailImage;
+  }
+
+  private static isValid(thumbnailMediaId: Option<MediaId>, shouldProcessThumbnailImage: boolean) {
+    const hasImageThumbnail = thumbnailMediaId === undefined || !(thumbnailMediaId instanceof NetmateImageId);
+    return shouldProcessThumbnailImage && hasImageThumbnail;
+  }
+
+  isSharer(id: HandleId): boolean {
+    return this.sharerId.asHexadecimalRepresentation() === id.asHexadecimalRepresentation();
+  }
+}
