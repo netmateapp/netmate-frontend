b09feaf38d72ba6e8533e639c1290dbecc32af32
amata1219 <suzukiamata@gmail.com>
2024-06-11 17:42:45 +0900

    追加: タグ関連の値オブジェクトを定義

---
+++ src/routes/tags/[id=uuidv4]/space/domain/tag.ts
@@ -0,0 +1,29 @@
+import { calculateCharactersCosts } from "$lib/cjk.svelte";
+import type { Uuid4 } from "$lib/uuid";
+
+export type TagId = Uuid4;
+
+const MAX_TAG_NAME_CHARACTERS_COST = 100;
+
+export class TagName {
+  public readonly name: string;
+
+  constructor(name: string) {
+    if (!TagName.isValid(name)) throw new Error(`A characters cost of name must be less than or equal to ${MAX_TAG_NAME_CHARACTERS_COST}.`);
+    this.name = name;
+  }
+
+  private static isValid(name: string): boolean {
+    return calculateCharactersCosts(name) <= MAX_TAG_NAME_CHARACTERS_COST;
+  }
+}
+
+export class Tag {
+  public readonly id: TagId;
+  public readonly name: TagName;
+
+  constructor(id: TagId, name: TagName) {
+    this.id = id;
+    this.name = name;
+  }
+}
