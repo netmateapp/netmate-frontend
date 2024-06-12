c50688c74b076c14fa542573f43487578a888b6d
amata1219 <suzukiamata@gmail.com>
2024-06-12 18:38:10 +0900

    追加: 仮想座標とHTML座標を表す値オブジェクト

---
+++ src/routes/tags/[id=uuidv4]/space/scripts/position.ts
@@ -0,0 +1,84 @@
+export class VirtualCoordinate {
+  public readonly coordinate: number;
+
+  private constructor(coordinate: number) {
+    if (!VirtualCoordinate.isValid(coordinate)) throw new Error(`A coordinate must be an integer.`);
+    this.coordinate = coordinate;
+  }
+
+  private static isValid(coordinate: number): boolean {
+    return Number.isInteger(coordinate);
+  }
+
+  static of(coordinate: number): VirtualCoordinate {
+    return new VirtualCoordinate(coordinate);
+  }
+}
+
+export class VirtualLocation {
+  public readonly x: VirtualCoordinate;
+  public readonly y: VirtualCoordinate;
+
+  private constructor(x: VirtualCoordinate, y: VirtualCoordinate) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static of(x: VirtualCoordinate, y: VirtualCoordinate): VirtualLocation {
+    return new VirtualLocation(x, y);
+  }
+}
+
+export const VIRTUAL_COORDINATE_SYSTEM_ORIGIN = VirtualLocation.of(VirtualCoordinate.of(0), VirtualCoordinate.of(0));
+
+export class ReactiveVirtualLocation {
+  private location: VirtualLocation = $state(VirtualLocation.of(VirtualCoordinate.of(0), VirtualCoordinate.of(0)));
+
+  constructor(initialLocation: VirtualLocation) {
+    this.location = initialLocation;
+  }
+
+  reactiveX(): VirtualCoordinate {
+    return this.location.x;
+  }
+
+  reactiveY(): VirtualCoordinate {
+    return this.location.y;
+  }
+
+  update(newLocation: VirtualLocation) {
+    this.location = newLocation;
+  }
+}
+
+export class HtmlCoordinate {
+  public readonly coordinate: number;
+
+  private constructor(coordinate: number) {
+    if (!HtmlCoordinate.isValid(coordinate)) throw new Error(`A coordinate must be an integer.`);
+    this.coordinate = coordinate;
+  }
+
+  private static isValid(coordinate: number): boolean {
+    return Number.isInteger(coordinate);
+  }
+
+  static of(coordinate: number): HtmlCoordinate {
+    return new HtmlCoordinate(coordinate);
+  }
+
+}
+
+export class HtmlLocation {
+  public readonly x: HtmlCoordinate;
+  public readonly y: HtmlCoordinate;
+
+  private constructor(x: HtmlCoordinate, y: HtmlCoordinate) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static of(x: HtmlCoordinate, y: HtmlCoordinate): HtmlLocation {
+    return new HtmlLocation(x, y);
+  }
+}
