de2cd701c9898a375b793663c94f911ec69ecf90
amata1219 <suzukiamata@gmail.com>
2024-06-13 13:35:59 +0900

    追加: 実座標の値オブジェクト

--- src/routes/tags/[id=uuidv4]/space/scripts/coordinateSystem.ts
+++ src/routes/tags/[id=uuidv4]/space/scripts/coordinateSystem.ts
@@ -1,3 +1,5 @@
+import type { Scale } from "./scale";
+
 export class VirtualCoordinate {
   public readonly coordinate: number;
 
@@ -83,5 +85,34 @@ export class HtmlLocation {
 
   static of(x: HtmlCoordinate, y: HtmlCoordinate): HtmlLocation {
     return new HtmlLocation(x, y);
+  }
+}
+
+export class RealCoordinate {
+  public readonly scale: Scale;
+  public readonly coordinate: number;
+
+  private constructor(htmlCoordinate: HtmlCoordinate, scale: Scale) {
+    this.scale = scale;
+    this.coordinate = htmlCoordinate.coordinate * scale.scale;
+  }
+}
+
+export class RealLocation {
+  public readonly x: RealCoordinate;
+  public readonly y: RealCoordinate;
+
+  private constructor(x: RealCoordinate, y: RealCoordinate) {
+    if (!RealLocation.isValid(x, y)) throw new Error(`The scales of x and y coordinates must match.`);
+    this.x = x;
+    this.y = y;
+  }
+
+  private static isValid(x: RealCoordinate, y: RealCoordinate) {
+    return x.scale.scale === y.scale.scale;
+  }
+
+  static of(x: RealCoordinate, y: RealCoordinate): RealLocation {
+    return new RealLocation(x, y);
   }
 }
