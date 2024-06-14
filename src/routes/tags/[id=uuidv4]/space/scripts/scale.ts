4d1118ef4e59bff6df083477bd89b316d22c5ff8
amata1219 <suzukiamata@gmail.com>
2024-06-14 21:19:41 +0900

    追加: スケールの更新処理

--- src/routes/tags/[id=uuidv4]/space/scripts/scale.ts
+++ src/routes/tags/[id=uuidv4]/space/scripts/scale.ts
@@ -1,10 +1,69 @@
+import type { Finalizer, LifeCycle } from "./lifeCycle";
+import type { Reactive, Reactivity } from "./reactivity";
+
 const MAX_SCALE = 1.0;
 const MIN_SCALE = 0.5;
 
-export class Scaler {
-  private scale: number = $state(MAX_SCALE);
+export class Scale {
+  public readonly scale: number;
 
-  currentScale(): number {
+  constructor(scale: number) {
+    if (!Scale.isValid(scale)) throw new Error(`A scale must be between ${MIN_SCALE} and ${MAX_SCALE}.`);
+    this.scale = scale;
+  }
+
+  private static isValid(scale: number): boolean {
+    return MIN_SCALE <= scale && scale <= MAX_SCALE;
+  }
+}
+
+const DEFAULT_SCALE = new Scale(MAX_SCALE);
+
+// 実座標はscale=1.0時のHTML座標 * scaleで決定される
+export class ReactiveScale implements Reactivity<Scale> {
+  private scale: Scale = $state(DEFAULT_SCALE);
+
+  constructor(scale: Scale) {
+    this.scale = scale;
+  }
+
+  reactiveValue(): Reactive<Scale> {
     return this.scale;
+  }
+
+  update(scale: Scale) {
+    this.scale = scale;
+  }
+}
+
+export class ScaleUpdater implements LifeCycle {
+  private readonly scale: ReactiveScale;
+  private static INCREMENTATION: number = 0.1;
+
+  constructor(scale: ReactiveScale) {
+    this.scale = scale;
+  }
+
+  onWheel(event: WheelEvent) {
+    if (event.deltaY > 0) this.tryScaleDown();
+    else this.tryScaleUp();
+  }
+
+  tryScaleUp() {
+    const currentScale = this.scale.reactiveValue().scale;
+    if (currentScale < MAX_SCALE) this.scale.update(new Scale(currentScale + ScaleUpdater.INCREMENTATION));
+  }
+
+  tryScaleDown() {
+    const currentScale = this.scale.reactiveValue().scale;
+    if (currentScale > MIN_SCALE) this.scale.update(new Scale(currentScale - ScaleUpdater.INCREMENTATION));
+  }
+
+  initialize(): Finalizer {
+    const onWheel = (event: WheelEvent) => this.onWheel(event);
+    document.addEventListener("wheel", onWheel);
+    return () => {
+      document.removeEventListener("wheel", onWheel);
+    };
   }
 }
