3877cc83ab5ef98c60378b06f0dcd206d6329ab5
amata1219 <suzukiamata@gmail.com>
2024-06-14 19:58:25 +0900

    追加: LifeCycleの合成関数

--- src/routes/tags/[id=uuidv4]/space/scripts/lifeCycle.ts
+++ src/routes/tags/[id=uuidv4]/space/scripts/lifeCycle.ts
@@ -1,25 +1,16 @@
-import type { Option } from "$lib/option";
-
 export type Finalizer = () => void;
 
 export interface LifeCycle {
   initialize(): Finalizer;
 }
 
-/*export function invokeInitializer(initializer: Initializer): Option<Finalizer> {
-  const returnValue: Finalizer | void = initializer();
-  const finalizer: Option<Finalizer> = returnValue !== undefined ? returnValue : undefined;
-  return finalizer;
-}
-
-export function combineInitializers(...initializers: Initializer[]): Initializer {
-  return () => {
-    const finalizers: Finalizer[] = initializers
-      .map(initializer => invokeInitializer(initializer))
-      .filter(option => option !== undefined);
-
-    return () => {
-      finalizers.forEach(finalizer => finalizer());
+export function composeLifeCycles(...lifeCycles: LifeCycle[]): LifeCycle {
+  return {
+    initialize: () => {
+      const finalizers: Finalizer[] = lifeCycles.map(lifeCycle => lifeCycle.initialize());
+      return () => {
+        finalizers.forEach(finalizer => finalizer());
+      };
     }
-  }
-}*/
+  };
+}
