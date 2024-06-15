c4e6eb95c8b066a793de6e136033a78756cc7e4b
amata1219 <suzukiamata@gmail.com>
2024-06-15 18:25:42 +0900

    追加: スペース上の共有クラスタ

---
+++ src/routes/tags/[id=uuidv4]/space/components/chunk/content/ShareCardsCluster.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import SpaceShareCard from "$lib/components/space/share/SpaceShareCard.svelte";
+  import type { ShareCardsCluster } from "../../../scripts/chunk/chunkContent";
+    import { RealCoordinate, RealLocation } from "../../../scripts/coordinateSystem/realCoordinateSystem";
+
+  type Props = {
+    shareCardsCluster: ShareCardsCluster
+  };
+
+  let { shareCardsCluster }: Props = $props();
+
+  function createRealLocation(index: number): RealLocation {
+    const offset = RealCoordinate.of(index === 0 ? 22 : 22 + 468 + 22);
+    return RealLocation.of(offset, offset);
+  }
+</script>
+
+{#each shareCardsCluster.shareCards as shareCard, index}
+  <SpaceShareCard location={createRealLocation(index)} shareCard={shareCard} />
+{/each}
