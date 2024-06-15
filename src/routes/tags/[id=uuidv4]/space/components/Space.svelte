29fc90ab870cb122a07f643fc3cc18e353be3ff0
amata1219 <suzukiamata@gmail.com>
2024-06-16 01:19:27 +0900

    追加: タグスペースのコンポーネント

---
+++ src/routes/tags/[id=uuidv4]/space/components/Space.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { Tag } from "$lib/scripts/domain/tag";
+  import { CHUNK_SIDE_LENGTH, ChunkLocation, ChunkRepository } from "../scripts/chunk/chunk";
+  import { VirtualCoordinate, VirtualLocation } from "../scripts/coordinateSystem/virtualCoordinateSystem";
+  import { MAX_SCALE, Scale } from "../scripts/scale";
+  import { TagSpace } from "../scripts/space";
+  import CenterChunk from "./chunk/CenterChunk.svelte";
+  import Chunk from "./chunk/Chunk.svelte";
+
+  let { tag }: { tag: Tag } = $props();
+
+  const initialLocation = VirtualLocation.of(
+    VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2),
+    VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2)
+  );
+
+  const space = new TagSpace(
+    tag,
+    new ChunkRepository(),
+    initialLocation,
+    new Scale(MAX_SCALE)
+  );
+
+  $effect(() => {
+    return space.initialize();
+  });
+
+  $effect(() => {
+    space.dynamicChunkRenderer.startDynamicChunkRendering(space.viewCenterLocation.reactiveValue());
+  });
+
+  function isCenterChunk(chunkLocation: ChunkLocation): boolean {
+    return chunkLocation.chunkX.coordinate === 0 && chunkLocation.chunkY.coordinate === 0;
+  }
+</script>
+
+<div class="space">
+  {#each space.renderedChunks.reactiveValue() as chunk}
+    {#if isCenterChunk(chunk.location)}
+      <CenterChunk {space} {chunk} />
+    {:else}
+      <Chunk {space} {chunk} />
+    {/if}
+  {/each}
+</div>
