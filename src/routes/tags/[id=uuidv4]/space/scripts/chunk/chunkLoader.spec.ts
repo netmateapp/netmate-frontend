80e2c061b1d7d79fc7494b43d7adb1eeb7031e84
amata1219 <suzukiamata@gmail.com>
2024-06-15 14:33:52 +0900

    追加: chunkLoaderのテスト

---
+++ src/routes/tags/[id=uuidv4]/space/scripts/chunk/chunkLoader.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+import { VirtualCoordinate, VirtualLocation } from "../coordinateSystem/virtualCoordinateSystem";
+import { ChunkLoader, TagSpaceChunkFetcher } from "./chunkLoader";
+import { ChunkCoordinate, ChunkLocation, ChunkRepository } from "./chunk";
+
+test("ChunkLoader#loadChunksInMovementDirection(): 方向性チャンク読み込み", () => {
+  const from = VirtualLocation.of(VirtualCoordinate.of(0), VirtualCoordinate.of(0));
+  const to = VirtualLocation.of(VirtualCoordinate.of(1), VirtualCoordinate.of(1));
+  const repository = new ChunkRepository();
+  const loader = new ChunkLoader(repository, new TagSpaceChunkFetcher());
+  loader.loadChunksInMovementDirection(from, to, 1, 1);
+  const hasEdgeChunk = repository.hasChunkAt(ChunkLocation.of(ChunkCoordinate.of(2), ChunkCoordinate.of(2)));
+  expect(hasEdgeChunk).toBe(true);
+});
