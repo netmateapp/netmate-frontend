796b65da52f7ff4d3f498587651000c3baa06361
amata1219 <suzukiamata@gmail.com>
2024-06-15 20:39:07 +0900

    修正: 共有カードを追加

--- src/routes/tags/[id=uuidv4]/space/components/chunk/content/SpaceCore.svelte
+++ src/routes/tags/[id=uuidv4]/space/components/chunk/content/SpaceCore.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import type { Reactive } from "$lib/scripts/extension/reactivity";
-    import type { SpaceCore } from "../../../scripts/chunk/chunkContent";
-    import type { LocationTransformer } from "../../../scripts/coordinateSystem/locationTransformer";
-    import { RealCoordinate, RealLocation } from "../../../scripts/coordinateSystem/realCoordinateSystem";
-    import type { ViewportWidth, ViewportHeight } from "../../../scripts/coordinateSystem/viewportRelativeCoordinateSystem";
-    import type { VirtualLocation } from "../../../scripts/coordinateSystem/virtualCoordinateSystem";
-    import type { Scale } from "../../../scripts/scale";
-    import LocationName from "./LocationName.svelte";
+  import SpaceShareCard from "$lib/components/space/share/SpaceShareCard.svelte";
+  import type { Reactive } from "$lib/scripts/extension/reactivity";
+  import type { SpaceCore } from "../../../scripts/chunk/chunkContent";
+  import type { LocationTransformer } from "../../../scripts/coordinateSystem/locationTransformer";
+  import { RealCoordinate, RealLocation } from "../../../scripts/coordinateSystem/realCoordinateSystem";
+  import type { ViewportWidth, ViewportHeight } from "../../../scripts/coordinateSystem/viewportRelativeCoordinateSystem";
+  import type { VirtualLocation } from "../../../scripts/coordinateSystem/virtualCoordinateSystem";
+  import type { Scale } from "../../../scripts/scale";
+  import LocationName from "./LocationName.svelte";
 
   type Props = {
     viewCenterLocation: Reactive<VirtualLocation>;
@@ -18,6 +19,12 @@
   };
 
   let { viewCenterLocation, viewportWidth, viewportHeight, scale, locationTransformer, spaceCore }: Props = $props();
+
+  function createRealLocation(index: number): RealLocation {
+    const offsetX = RealCoordinate.of(index === 0 ? -2 : 510);
+    const offsetY = RealCoordinate.of(index === 0 ? 254 : 430);
+    return RealLocation.of(offsetX, offsetX);
+  }
 </script>
 
 <a
@@ -26,4 +33,7 @@
   style="top: {22}px; left: {22}px;">
   <div class="shadow-overlay"></div>
   <LocationName tag={spaceCore.tag} relativeLocation={RealLocation.of(RealCoordinate.of(488), RealCoordinate.of(160))} />
+  {#each spaceCore.shareCardsCluster.shareCards as shareCard, index}
+    <SpaceShareCard location={createRealLocation(index)} shareCard={shareCard} />
+  {/each}
 </a>
