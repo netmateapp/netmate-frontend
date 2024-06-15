e51b4278ab60e8c11c018eeea15d2f5af24c1bc8
amata1219 <suzukiamata@gmail.com>
2024-06-16 02:29:35 +0900

    追加: タグスペースコアのCSS

--- src/routes/tags/[id=uuidv4]/space/components/chunk/content/SpaceCore.svelte
+++ src/routes/tags/[id=uuidv4]/space/components/chunk/content/SpaceCore.svelte
@@ -29,3 +29,28 @@
     <SpaceShareCard location={createRealLocation(index)} shareCard={shareCard} />
   {/each}
 </a>
+
+<style>
+  .space-core {
+    position: absolute;
+    width: 61rem;
+    height: 61rem;
+    flex-shrink: 0;
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, 0.01);
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  .shadow-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    box-shadow: 1px 2px 8px 0px rgba(0, 0, 0, 0.16) inset;
+    z-index: 1;
+    pointer-events: none;
+  }
+</style>
