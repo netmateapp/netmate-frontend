53e33512c2e5cde2f763c9a5c672bd5c752de2fd
amata1219 <suzukiamata@gmail.com>
2024-06-16 01:18:04 +0900

    修正: プロパティをTagSpaceで置き換え

--- src/routes/tags/[id=uuidv4]/space/components/chunk/CenterChunk.svelte
+++ src/routes/tags/[id=uuidv4]/space/components/chunk/CenterChunk.svelte
@@ -1,40 +1,32 @@
 <script lang="ts">
-  import type { Tag } from "$lib/scripts/domain/tag";
-  import type { Reactive } from "$lib/scripts/extension/reactivity";
   import { CHUNK_SIDE_LENGTH, type Chunk } from "../../scripts/chunk/chunk";
-  import { ShareCardsClusterData, SpaceCoreData } from "../../scripts/chunk/chunkContent";
-  import type { LocationTransformer } from "../../scripts/coordinateSystem/locationTransformer";
+  import { ShareCardsClusterData } from "../../scripts/chunk/chunkContent";
   import { REAL_COORDINATE_SYSTEM_ORIGIN, RealCoordinate, RealLocation } from "../../scripts/coordinateSystem/realCoordinateSystem";
-  import type { ViewportHeight, ViewportWidth } from "../../scripts/coordinateSystem/viewportRelativeCoordinateSystem";
   import { VirtualLocation } from "../../scripts/coordinateSystem/virtualCoordinateSystem";
-  import type { Scale } from "../../scripts/scale";
+  import type { TagSpace } from "../../scripts/space";
   import LocationName from "./content/LocationName.svelte";
   import ShareCardsCluster from "./content/ShareCardsCluster.svelte";
   import SpaceCore from "./content/SpaceCore.svelte";
 
   type Props = {
-    viewCenterLocation: Reactive<VirtualLocation>;
-    viewportWidth: Reactive<ViewportWidth>;
-    viewportHeight: Reactive<ViewportHeight>;
-    scale: Reactive<Scale>;
-    locationTransformer: LocationTransformer;
+    space: TagSpace;
     chunk: Chunk;
-    tag: Tag;
   };
 
-  let { viewCenterLocation, viewportWidth, viewportHeight, scale, locationTransformer, chunk, tag }: Props = $props();
+  let { space, chunk }: Props = $props();
 
   let realLocation: RealLocation = REAL_COORDINATE_SYSTEM_ORIGIN;
 
   $effect(() => {
-    realLocation = locationTransformer.transformToRealLocation(
-      viewCenterLocation,
+    realLocation = space.locationTransformer.transformToRealLocation(
+      space.viewCenterLocation.reactiveValue(),
       VirtualLocation.fromChunkLocation(chunk.location),
-      viewportWidth,
-      viewportHeight,
-      scale
+      space.viewportWidth.reactiveValue(),
+      space.viewportHeight.reactiveValue(),
+      space.scale.reactiveValue()
     );
-  })
+  });
+
 
   function bottomStyle(): number {
     return realLocation.y.coordinate;
@@ -49,7 +41,7 @@
   }
 
   function scaleStyle(): number {
-    return scale.scale;
+    return space.scale.reactiveValue().scale;
   }
 
   function hasShareCardsCluster(): boolean {
@@ -64,7 +56,7 @@
 <div
   class="chunk"
   style="bottom: {bottomStyle()}px; left: {leftStyle()}px; width: {sizeStyle()}px; height: {sizeStyle()}px; scale: {scaleStyle()};">
-  <LocationName {tag} relativeLocation={RealLocation.of(RealCoordinate.of(512), RealCoordinate.of(40))} />
+  <LocationName tag={space.tag} relativeLocation={RealLocation.of(RealCoordinate.of(512), RealCoordinate.of(40))} />
   {#if hasShareCardsCluster()}
     <ShareCardsCluster shareCardsCluster={chunk.content as ShareCardsClusterData} />
   {/if}
