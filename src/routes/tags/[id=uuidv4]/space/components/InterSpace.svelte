ad89de081efbad66ce6e1e96ddd498648cad2663
amata1219 <suzukiamata@gmail.com>
2024-06-19 18:39:19 +0900

    整理: アニメーション関連処理のサブコンポーネント化

--- src/routes/tags/[id=uuidv4]/space/components/InterSpace.svelte
+++ src/routes/tags/[id=uuidv4]/space/components/InterSpace.svelte
@@ -2,12 +2,16 @@
   import Space from "./Space.svelte";
   import { Tag } from "$lib/scripts/domain/tag";
   import type { Option } from "$lib/option";
-  import { VIRTUAL_COORDINATE_SYSTEM_ORIGIN, VirtualCoordinate, VirtualLocation } from "../scripts/coordinateSystem/virtualCoordinateSystem.svelte";
+  import { VirtualCoordinate, VirtualLocation } from "../scripts/coordinateSystem/virtualCoordinateSystem.svelte";
   import { CHUNK_SIDE_LENGTH, Chunk, ChunkRepository } from "../scripts/chunk/chunk";
   import { TagSpace } from "../scripts/space";
   import { MAX_SCALE, Scale } from "../scripts/scale.svelte";
   import { SpaceCoreData } from "../scripts/chunk/chunkContent";
   import { REAL_COORDINATE_SYSTEM_ORIGIN, RealLocation } from "../scripts/coordinateSystem/realCoordinateSystem";
+  import { SessionMemory } from "$lib/scripts/memory/sessionMemory";
+  import { SpaceHistory } from "../scripts/cache/spaceHisotry";
+    import TransferAnimation from "./TransferAnimation.svelte";
+    import type { SvelteComponent } from "svelte";
 
   type Props = {
     tag: Tag;
@@ -15,7 +19,13 @@
 
   let { tag }: Props = $props();
 
-  let isTransiting: boolean = $state(false);
+  const history = SessionMemory.of("space-tag-history", new SpaceHistory());
+
+  $effect(() => {
+    return history.initialize();
+  });
+
+  let isUserTransferring: boolean = $state(false);
 
   let currentSpace: TagSpace = $state(
     new TagSpace(
@@ -28,31 +38,32 @@
       new Scale(MAX_SCALE)
     )
   );
-  let nextSpace: Option<TagSpace> = $state(undefined);
-  let spaceCoreRelativeLocation: VirtualLocation;
+  let transferSpace: Option<TagSpace> = $state(undefined);
+  let transferSpaceViewCenterLocation: VirtualLocation;
+
+  function findClickedSpaceCoreChunk(tag: Tag): Option<Chunk> {
+    for (var renderdChunk of currentSpace.renderedChunks.reactiveValue()) {
+      if (renderdChunk.content instanceof SpaceCoreData && renderdChunk.content.tag.equals(tag)) {
+        return renderdChunk;
+      }
+    }
+    return undefined;
+  }
 
   $effect(() => {
-    if (currentSpace.tag.id.asHexadecimalRepresentation() !== tag.id.asHexadecimalRepresentation() && !isTransiting) {
-      let spaceCoreChunk: Option<Chunk> = undefined;
-      for (var chunk of currentSpace.renderedChunks.reactiveValue()) {
-        if (chunk.content instanceof SpaceCoreData) {
-          if (chunk.content.tag.id.asHexadecimalRepresentation() === tag.id.asHexadecimalRepresentation()) {
-            spaceCoreChunk = chunk;
-            break;
-          }
-        }
-      }
+    if (isUserTransferring || currentSpace.tag.equals(tag)) return;
 
+    let spaceCoreChunk: Option<Chunk> = findClickedSpaceCoreChunk(tag);
       if (spaceCoreChunk === undefined) return;
       
       const viewCenter = currentSpace.viewCenterLocation.reactiveValue();
       const spaceCoreChunkCenterLocation: VirtualLocation = spaceCoreChunk.centerLocation();
-      spaceCoreRelativeLocation = viewCenter.createOffsetLocation(
+      transferSpaceViewCenterLocation = viewCenter.createOffsetLocation(
         VirtualCoordinate.of(-spaceCoreChunkCenterLocation.x.coordinate),
         VirtualCoordinate.of(-spaceCoreChunkCenterLocation.y.coordinate)
       );
 
-      spaceCoreChunkLocaiton = currentSpace.locationTransformer.transformToRealLocation(
+      virtualChunkLocation = currentSpace.locationTransformer.transformToRealLocation(
         currentSpace.viewCenterLocation.reactiveValue(),
         VirtualLocation.fromChunkLocation(spaceCoreChunk.location),
         currentSpace.viewportWidth.reactiveValue(),
@@ -60,137 +71,41 @@
         currentSpace.scale.reactiveValue()
       );
 
-      nextSpace = new TagSpace(
+      transferSpace = new TagSpace(
         tag,
         new ChunkRepository(),
-        VirtualLocation.of(
-          VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2),
-          VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2)
-        ),
+        VirtualLocation.of(VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2), VirtualCoordinate.of(CHUNK_SIDE_LENGTH / 2)),
         new Scale(MAX_SCALE)
-      )
+      );
+
       startTransition();
-    }
   });
 
-  let clipPathFrom: number = $derived(30.5);
-  let clipPathTo: number = $derived(61 * (1 / currentSpace.scale.reactiveValue().scale));
-  let scaleTo: number = $derived(2 * (1 / currentSpace.scale.reactiveValue().scale));
-  let spaceCoreChunkLocaiton: RealLocation = REAL_COORDINATE_SYSTEM_ORIGIN;
-  let spaceCoreOverlayRef: Option<HTMLElement> = $state(undefined);
-  let centeredSpaceRef: Option<HTMLElement> = $state(undefined);
-  let backgroundRef: Option<HTMLElement> = $state(undefined);
+  let virtualChunkLocation: RealLocation = REAL_COORDINATE_SYSTEM_ORIGIN;
+  let transferAnimation: Option<SvelteComponent> = $state(undefined);
+  
   function startTransition() {
-    isTransiting = true;
+    isUserTransferring = true;
 
     setTimeout(() => {
-      if (centeredSpaceRef !== undefined) centeredSpaceRef.style.clipPath = `circle(${clipPathTo}rem)`;
-      if (backgroundRef !== undefined) {
-        backgroundRef.style.clipPath = `circle(${clipPathTo}rem)`;
-        backgroundRef.style.backgroundColor = "white";
-      }
-      if (spaceCoreOverlayRef !== undefined) spaceCoreOverlayRef.style.scale = `${scaleTo}`;
+      transferAnimation?.animate();
     }, 0);
 
     setTimeout(() => {
-      nextSpace?.scale.update(currentSpace.scale.reactiveValue());
-      currentSpace = nextSpace!;
+      transferSpace?.scale.update(currentSpace.scale.reactiveValue());
+      currentSpace = transferSpace!;
       const viewCenter = currentSpace.viewCenterLocation.reactiveValue().createOffsetLocation(
-        spaceCoreRelativeLocation.x,
-        spaceCoreRelativeLocation.y
+        transferSpaceViewCenterLocation.x,
+        transferSpaceViewCenterLocation.y
       );
       currentSpace.viewCenterLocation.update(viewCenter);
 
-      isTransiting = false;
-      spaceCoreOverlayRef = undefined;
+      isUserTransferring = false;
     }, 200);
   }
 </script>
 
 <Space space={currentSpace} />
-{#if isTransiting}
-  <div class="virtual-chunk" style="bottom: {spaceCoreChunkLocaiton.y.coordinate}px; left: {spaceCoreChunkLocaiton.x.coordinate}px; scale: {currentSpace.scale.reactiveValue().scale};">
-    <div class="expanding-space-wrapper">
-      <div bind:this={spaceCoreOverlayRef} class="space-core-overlay" style="--overlay-size: {61}rem; --scale-to: {scaleTo};"></div>
-      <div bind:this={centeredSpaceRef} class="centered-space" style="--from-clip-path: {clipPathFrom}rem; --to-clip-path: {clipPathTo}rem;">
-        <Space space={nextSpace!} />
-        <div bind:this={backgroundRef} class="background" style="--from-clip-path: {clipPathFrom}rem; --to-clip-path: {clipPathTo}rem;"></div>
-      </div>
-  </div>
-</div>
+{#if isUserTransferring}
+  <TransferAnimation bind:this={transferAnimation} currentSpaceScale={currentSpace.scale.reactiveValue()} nextSpace={transferSpace!} {virtualChunkLocation} />
 {/if}
-
-<style>
-  .virtual-chunk {
-    position: absolute;
-    width: 1024px;
-    height: 1024px;
-    display: grid;
-    place-content: center;
-    z-index: 2;
-  }
-
-  .expanding-space-wrapper {
-    display: grid;
-    place-content: center;
-    z-index: 2;
-  }
-
-  .centered-space {
-    width: 100vw;
-    height: 100vh;
-    clip-path: circle(var(--from-clip-path));
-    transition: clip-path 0.2s linear;
-    display: grid;
-    place-content: center;
-  }
-
-  .background {
-    width: 1000vmax;
-    height: 1000vmax;
-    clip-path: circle(var(--from-clip-path));
-    background-color: #fcfcfc;
-    transition: clip-path 0.2s linear, background-color 0.2s linear;
-  }
-
-  .space-core-overlay {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    translate: -50% -50%;
-    width: var(--overlay-size);
-    height: var(--overlay-size);
-    box-shadow: 1px 2px 8px 0px rgba(0, 0, 0, 0.16) inset;
-    border-radius: 50%;
-    pointer-events: none;
-    z-index: 2;
-    transition: scale 0.2s linear;
-  }
-
-  @keyframes scaleShadowOverlay {
-    from {
-      transform: scale(1.0);
-    }
-    to {
-      transform: scale(var(--scale-to));
-    }
-  }
-
-  @keyframes expandNewSpace {
-    from {
-      clip-path: circle(var(--from-clip-path));
-    }
-    to {
-      clip-path: circle(var(--to-clip-path));
-    }
-  }
-
-  @keyframes backShadow {
-    from {
-      background-color: #fcfcfc;
-    }
-    to {
-      background-color: white;
-    }
-  }
-</style>
