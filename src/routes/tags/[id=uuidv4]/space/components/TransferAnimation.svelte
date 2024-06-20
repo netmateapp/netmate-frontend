3564abd65a3c868b6eb40c0151da11ad937651bb
amata1219 <suzukiamata@gmail.com>
2024-06-20 21:34:44 +0900

    修正: スペースコア全体に陰がかかるように

--- src/routes/tags/[id=uuidv4]/space/components/TransferAnimation.svelte
+++ src/routes/tags/[id=uuidv4]/space/components/TransferAnimation.svelte
@@ -15,8 +15,8 @@
 
   const CLIP_PATH_FROM: number = 30.5;
   let clipPathTo: number = $derived(61 * (1 / currentSpaceScale.scale));
-  const BACKGROUND_START_COLOR: string = "fcfcfc";
-  const BACKGROUND_END_COLOR: string = "ffffff";
+  const BACKGROUND_START_COLOR: string = "rgba(0, 0, 0, 0.01)";
+  const BACKGROUND_END_COLOR: string = "rgba(0, 0, 0, 0)";
   const SCALE_FROM: number = 1.0;
   let scaleTo: number = $derived(2 * (1 / currentSpaceScale.scale));
   let spaceCoreOverlayRef: Option<HTMLElement> = $state(undefined);
@@ -25,11 +25,11 @@
 
   export function animate() {
     if (centeredSpaceRef !== undefined) centeredSpaceRef.style.clipPath = `circle(${clipPathTo}rem)`;
-    if (backgroundRef !== undefined) {
-      backgroundRef.style.clipPath = `circle(${clipPathTo}rem)`;
-      backgroundRef.style.backgroundColor = BACKGROUND_END_COLOR;
+    if (backgroundRef !== undefined) backgroundRef.style.clipPath = `circle(${clipPathTo}rem)`;
+    if (spaceCoreOverlayRef !== undefined) {
+      spaceCoreOverlayRef.style.scale = `${scaleTo}`;
+      spaceCoreOverlayRef.style.backgroundColor = BACKGROUND_END_COLOR;
     }
-    if (spaceCoreOverlayRef !== undefined) spaceCoreOverlayRef.style.scale = `${scaleTo}`;
   }
 </script>
 
@@ -38,7 +38,7 @@
     <div
       bind:this={spaceCoreOverlayRef}
       class="space-core-overlay"
-      style="--scale-from: {SCALE_FROM}rem; --scale-to: {scaleTo};"></div>
+      style="--scale-from: {SCALE_FROM}rem; --scale-to: {scaleTo}; --bg-start-color: {BACKGROUND_START_COLOR}; --bg-end-color: {BACKGROUND_END_COLOR};"></div>
     <div
       bind:this={centeredSpaceRef}
       class="centered-space"
@@ -47,7 +47,7 @@
       <div
         bind:this={backgroundRef}
         class="background"
-        style="--clip-path-from: {CLIP_PATH_FROM}rem; --clip-path-to: {clipPathTo}rem; --bg-start-color: #{BACKGROUND_START_COLOR}; --bg-end-color: #{BACKGROUND_END_COLOR};"></div>
+        style="--clip-path-from: {CLIP_PATH_FROM}rem; --clip-path-to: {clipPathTo}rem;"></div>
     </div>
   </div>
 </div>
@@ -81,8 +81,8 @@
     width: 1000vmax;
     height: 1000vmax;
     clip-path: circle(var(--clip-path-from));
-    background-color: var(--bg-start-color);
-    transition: clip-path 0.2s linear, background-color 0.2s linear;
+    background-color: white;
+    transition: clip-path 0.2s linear;
   }
 
   .space-core-overlay {
@@ -93,11 +93,12 @@
     width: 61rem;
     height: 61rem;
     box-shadow: 1px 2px 8px 0px rgba(0, 0, 0, 0.16) inset;
+    background-color: var(--bg-start-color);
     border-radius: 50%;
     pointer-events: none;
     z-index: 2;
     scale: var(--scale-from);
-    transition: scale 0.2s linear;
+    transition: scale 0.2s linear, background-color 0.2s linear;
   }
 
   @keyframes scaleShadowOverlay {
