c0c9575e0e20011b7ee4674701609776c0dc90b5
amata1219 <suzukiamata@gmail.com>
2024-06-20 21:01:00 +0900

    削除: 不要ファイル

--- src/lib/scripts/extension/serialization.ts
+++
@@ -1,4 +0,0 @@
-import type { Option } from "$lib/option";
-
-export type Serializer<Value, Format> = (value: Value) => Option<Format>;
-export type Deserializer<Value, Format> = (format: Format) => Option<Value>;
