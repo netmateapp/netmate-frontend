2ef093732e92ae87e2e4fb150373810cb6aefe2c
amata1219 <suzukiamata@gmail.com>
2024-06-21 21:09:10 +0900

    追加: ヘッダーコンポーネント

---
+++ src/lib/components/common/header/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Brand from "../brand/Brand.svelte";
+  import Navigation from "../navigation/Navigation.svelte";
+  import SearchBox from "../search-box/SearchBox.svelte";
+</script>
+
+<Brand x={16} y={8} />
+<div class="search-box-wrapper">
+  <SearchBox />
+</div>
+<Navigation />
+
+<style>
+  .search-box-wrapper {
+    position: fixed;
+    top: 0.5rem;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    z-index: 100;
+  }
+</style>
