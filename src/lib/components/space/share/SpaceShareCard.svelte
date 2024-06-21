644b55512ef47c50957667b18b50512e3649f5a4
amata1219 <suzukiamata@gmail.com>
2024-06-21 14:58:46 +0900

    追加: 画像のぼかし機能

--- src/lib/components/space/share/SpaceShareCard.svelte
+++ src/lib/components/space/share/SpaceShareCard.svelte
@@ -14,6 +14,7 @@
   let { shareCard, location }: Props = $props();
 
   type WithinOneWeek = { unit: string; t: number };
+
   type ThisYear = {
     unit: string;
     month: number;
@@ -21,6 +22,7 @@
     hours: string;
     minutes: string;
   };
+
   type BeforeThisYear = {
     unit: string;
     year: number;
@@ -78,10 +80,86 @@
       return { unit: "minutes", t: minutes };
     }
   }
+
+  const MAX_BLUR = 14; // ぼかしの最大値
+  const MAX_GRAYSCALE = 0.3; // グレースケールの最大値
+  const DURATION = 500;
+  const INTERVAL = 100; // 100msごとのインターバル
+  const STEPS = DURATION / INTERVAL;
+  const BLUR_STEP = MAX_BLUR / STEPS;
+  const GRAYSCALE_STEP = MAX_GRAYSCALE / STEPS;
+
+  let intervalId: any;
+  let blurValue = MAX_BLUR;
+  let grayscaleValue = MAX_GRAYSCALE;
+  let isMouseDown = false;
+  let step = 0;
+
+  let imageRequireProcessing: HTMLImageElement;
+
+  function resetFilter() {
+    if (!isMouseDown) return;
+    isMouseDown = false;
+    clearInterval(intervalId); // 既存のインターバルをクリア
+    intervalId = setInterval(() => {
+      blurValue = Math.min(MAX_BLUR, blurValue + BLUR_STEP * 5); // 回復速度はぼかしを取る速度の5倍
+      grayscaleValue = Math.min(
+        MAX_GRAYSCALE,
+        grayscaleValue + GRAYSCALE_STEP * 5,
+      ); // 回復速度はぼかしを取る速度の5倍
+
+      // 移動で消失した場合
+      if (imageRequireProcessing === null) {
+        clearInterval(intervalId);
+        step = 0;
+        return;
+      }
+
+      imageRequireProcessing!.style.filter = `blur(${blurValue}px) grayscale(${grayscaleValue})`;
+      if (blurValue === MAX_BLUR && grayscaleValue === MAX_GRAYSCALE) {
+        clearInterval(intervalId);
+        step = 0;
+      }
+    }, INTERVAL); // INTERVALごとにぼかしとグレースケールを増やす
+  }
+
+  function onMouseDown() {
+    isMouseDown = true;
+    clearInterval(intervalId); // 既存のインターバルをクリア
+    intervalId = setInterval(() => {
+      step++;
+      blurValue = Math.max(0, blurValue - BLUR_STEP);
+      grayscaleValue = Math.max(0, grayscaleValue - GRAYSCALE_STEP);
+
+      if (imageRequireProcessing === null) {
+        clearInterval(intervalId);
+        step = 0;
+        return;
+      }
+
+      imageRequireProcessing!.style.filter = `blur(${blurValue}px) grayscale(${grayscaleValue})`;
+      if (blurValue === 0 && grayscaleValue === 0) {
+        clearInterval(intervalId);
+      }
+    }, INTERVAL); // INTERVALごとにぼかしとグレースケールを減らす
+  }
+
+  function onImageClick(event: MouseEvent) {
+    if (step > 1) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
 </script>
 
-<div class="share-wrapper" style="top: {location.y.coordinate}px; left: {location.x.coordinate}px;">
-  <a href="https://netmate.app/shares/{shareCard.sharerId.asHexadecimalRepresentation()}" class="share">
+<div
+  class="share-wrapper"
+  style="top: {location.y.coordinate}px; left: {location.x.coordinate}px;"
+>
+  <a
+    href="https://netmate.app/shares/{shareCard.sharerId.asHexadecimalRepresentation()}"
+    class="share"
+  >
     <div class="content">
       <div class="texts">
         {#if shareCard.title !== undefined}
@@ -94,23 +172,44 @@
       {#if shareCard.thumbnailMediaId !== undefined}
         <div class="media">
           {#if shareCard.hasImage()}
-            <img src={shareCard.thumbnailMediaId.id} />
+            {#if shareCard.shouldProcessThumbnailImage}
+              <img
+                bind:this={imageRequireProcessing}
+                src={shareCard.thumbnailMediaId.id}
+                class="should-process"
+                onmousedown={() => onMouseDown()}
+                onmouseup={() => resetFilter()}
+                onmouseleave={() => resetFilter()}
+                onmousemove={(event) => {
+                  if (event.buttons === 0) resetFilter();
+                }}
+                ondragstart={(event) => event.preventDefault()}
+                onclick={(event) => onImageClick(event)}
+              />
+            {:else}
+              <img src={shareCard.thumbnailMediaId.id} />
+            {/if}
           {:else if shareCard.hasSoundCloudAudio()}
             <iframe
               title="SoundCloud audio player"
               scrolling="no"
               frameborder="no"
               allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/{shareCard.thumbnailMediaId.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/{shareCard
+                .thumbnailMediaId
+                .id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            >
             </iframe>
           {:else if shareCard.hasYouTubeVideo()}
             <iframe
-              src="https://www.youtube-nocookie.com/embed/{shareCard.thumbnailMediaId.id}"
+              src="https://www.youtube-nocookie.com/embed/{shareCard
+                .thumbnailMediaId.id}"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen>
+              allowfullscreen
+            >
             </iframe>
           {/if}
         </div>
@@ -118,8 +217,16 @@
     </div>
     <div class="footer">
       <div class="information">
-        <span class="conversations-count">{_("conversations-count", { count: shareCard.conversationsCount.count })}</span>
-        <span class="timestamp">{_("timestamp", { ...elapsedTime(shareCard.timestamp.unixTimeMillis.time) })}</span>
+        <span class="conversations-count"
+          >{_("conversations-count", {
+            count: shareCard.conversationsCount.count,
+          })}</span
+        >
+        <span class="timestamp"
+          >{_("timestamp", {
+            ...elapsedTime(shareCard.timestamp.unixTimeMillis.time),
+          })}</span
+        >
       </div>
       <div class="more-button" use:tooltip={_("more-button-tooltip")}>
         <svg class="more-button-icon">
@@ -166,7 +273,7 @@
     grid-template-rows: auto 1fr;
     overflow: hidden;
   }
-  
+
   .texts {
     width: 100%;
     display: grid;
@@ -196,12 +303,19 @@
     height: 100%;
     min-height: 15.625rem;
     text-align: center;
+    overflow: hidden;
+    border-radius: 1rem;
   }
 
   .media img {
     max-width: 100%;
     height: 100%;
     border-radius: 1rem;
+  }
+
+  .should-process {
+    filter: blur(14px) grayscale(0.3);
+    transition: filter 0.1s linear;
   }
 
   .media iframe {
