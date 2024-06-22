7066d1104e9ee2d663f81c3d2276cc143e643e44
amata1219 <suzukiamata@gmail.com>
2024-06-23 01:20:17 +0900

    追加: 検索結果ページ

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Header from "$lib/components/common/header/Header.svelte";
+  import SharesList from "$lib/components/database/shares-list/SharesList.svelte";
+    import QueryDisplay from "./QueryDisplay.svelte";
+
+  function searchQuery(): string {
+    return "#早瀬ユウカ #イラスト";
+  }
+</script>
+
+<Header />
+<SharesList />
+
+<div class="side-bar">
+  <QueryDisplay query={searchQuery()} />
+</div>
+
+<style>
+  .side-bar {
+    position: fixed;
+    top: 6rem;
+    left: 0.75rem;
+    height: calc(100% - 6rem);
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    z-index: 100;
+  }
+</style>
