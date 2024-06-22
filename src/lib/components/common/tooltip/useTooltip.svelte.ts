a2024db333509d4c8b6935b86049f67c4b9156d2
amata1219 <suzukiamata@gmail.com>
2024-06-22 12:23:57 +0900

    追加: ツールチップを遅延して表示する機能

--- src/lib/components/common/tooltip/useTooltip.svelte.ts
+++ src/lib/components/common/tooltip/useTooltip.svelte.ts
@@ -4,6 +4,7 @@ export class TooltipData {
   y: number;
   targetElementHeight: number;
   show: boolean;
+  timeoutId: any;
 
   constructor(text: string, x: number, y: number, targetElementHeight: number, show: boolean) {
     this.text = text;
@@ -25,7 +26,48 @@ function showTooltip(text: string, x: number, y: number, targetElementHeight: nu
 }
 
 export function hideTooltip() {
+  cancelTimeout();
   tooltipData = new TooltipData("", 0, 0, 0, false);
+}
+
+function cancelTimeout() {
+  if (tooltipData.timeoutId !== undefined) {
+    clearTimeout(tooltipData.timeoutId);
+    tooltipData.timeoutId = undefined;
+  }
+}
+
+export function delayTooltip(targetNode: Element, props: { tooltipText: string, delay: number }) {
+  const show = (event: Event) => {
+    const rect = (event.target as Element).getBoundingClientRect();
+
+    cancelTimeout();
+    
+    tooltipData.timeoutId = setTimeout(() => {
+      showTooltip(
+        props.tooltipText,
+        rect.left + window.scrollX + rect.width / 2,
+        rect.top + window.scrollY,
+        rect.height
+      );
+    }, props.delay);
+  };
+
+  const hide = () => hideTooltip();
+
+  targetNode.addEventListener('mouseenter', show);
+  targetNode.addEventListener('mouseleave', hide);
+  targetNode.addEventListener('focus', show);
+  targetNode.addEventListener('blur', hide);
+
+  return {
+    destroy() {
+      targetNode.removeEventListener('mouseenter', show);
+      targetNode.removeEventListener('mouseleave', hide);
+      targetNode.removeEventListener('focus', show);
+      targetNode.removeEventListener('blur', hide);
+    }
+  }
 }
 
 export function tooltip(targetNode: Element, tooltipText: string) {
