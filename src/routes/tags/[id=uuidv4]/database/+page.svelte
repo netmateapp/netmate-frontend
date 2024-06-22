1c7b3698780898954e0e9bbaa4a4625c0f45bbea
amata1219 <suzukiamata@gmail.com>
2024-06-22 22:25:29 +0900

    追加: サイドバーを追加

--- src/routes/tags/[id=uuidv4]/database/+page.svelte
+++ src/routes/tags/[id=uuidv4]/database/+page.svelte
@@ -1,11 +1,28 @@
 <script>
-  import Brand from "$lib/components/common/brand/Brand.svelte";
-  import Navigation from "$lib/components/common/navigation/Navigation.svelte";
-  import SearchBox from "$lib/components/common/search-box/SearchBox.svelte";
+  import Header from "$lib/components/common/header/Header.svelte";
+  import LocationDisplay from "$lib/components/common/location-display/LocationDisplay.svelte";
   import SharesList from "$lib/components/database/shares-list/SharesList.svelte";
+  import TagMenu from "$lib/components/tag/TagMenu.svelte";
 </script>
 
-<Brand x={16} y={8} />
-<SearchBox />
-<Navigation />
+<Header />
 <SharesList />
+
+<div class="side-bar">
+  <LocationDisplay locationName={"ブルーアーカイブ"} isSpace={false} />
+  <TagMenu />
+</div>
+
+<style>
+  .side-bar {
+    position: fixed;
+    top: 6rem;
+    left: 0.75rem;
+    height: calc(100% - 6rem);
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    z-index: 100;
+  }
+</style>
