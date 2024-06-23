867fc06aea6908bf4af3ccc808e4bfdd23655033
amata1219 <suzukiamata@gmail.com>
2024-06-23 20:54:23 +0900

    追加: 共有情報コンポーネント

---
+++ src/routes/sessions/[id=uuidv7]/menu/ShareInformation.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  type Props = {
+    share: ShareData;
+  };
+
+  let { share }: Props = $props();
+
+  function leadSentence(): string {
+    return share.text !== undefined ? share.text.slice(0, 64) : "";
+  }
+</script>
+
+<div class="information">
+  <div class="spacer"></div>
+  {#if share.title !== undefined}
+    <span class="title">{share.title}</span>
+  {:else}
+    <span class="lead-sentence">{leadSentence()}</span>
+  {/if}
+</div>
+
+<style>
+  .information {
+    display: flex;
+    width: 11.125rem;
+    align-items: flex-start;
+  }
+
+  .spacer {
+    max-width: 0.5rem;
+    flex: 1 0 0;
+    align-self: stretch;
+  }
+
+  .title {
+    color: var(--secondary-color);
+    font-family: var(--primary-font);
+    font-size: 1.375rem;
+    line-height: 1.875rem;
+    max-width: 12.5rem;
+  }
+
+  .lead-sentence {
+    overflow: hidden;
+    color: var(--secondary-color);
+    text-overflow: ellipsis;
+    font-family: var(--primary-font);
+    font-size: 0.9375rem;
+    line-height: 1.25rem;
+    display: -webkit-box;
+    max-width: 12.5rem;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+  }
+</style>
