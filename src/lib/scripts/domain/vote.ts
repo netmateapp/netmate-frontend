46d375ab2b02ca5fac00c666a175d1cd4d87a84d
amata1219 <suzukiamata@gmail.com>
2024-06-23 17:02:16 +0900

    追加: 投票に関するモデル

---
+++ src/lib/scripts/domain/vote.ts
@@ -0,0 +1,16 @@
+export enum Vote {
+  Agree = "agree",
+  LittleAgree = "little-agree",
+  Disagree = "disagree",
+}
+
+export enum Rating {
+  HighRating,
+  Rating,
+  LowRating,
+}
+
+export enum TallyStatus {
+  Suggested,
+  Stabilized,
+}
