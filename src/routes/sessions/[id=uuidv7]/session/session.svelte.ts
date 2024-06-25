50a48b1559bcf73f3062f10de9f3cb41d06f8962
amata1219 <suzukiamata@gmail.com>
2024-06-25 23:26:52 +0900

    追加: セッション中の要素に関するクラス

--- src/routes/sessions/[id=uuidv7]/session/session.svelte.ts
+++ src/routes/sessions/[id=uuidv7]/session/session.svelte.ts
@@ -1,3 +1,22 @@
 import { createTranslator } from "$lib/i18n.svelte";
+import type { SessionShareData } from "$lib/scripts/domain/session";
 
 export const _ = createTranslator("session", "session");
+
+export class HiddenShares {
+  public readonly shares: SessionShareData[];
+
+  constructor(shares: SessionShareData[]) {
+    this.shares = shares;
+  }
+}
+
+export class DeletedSharesCount {
+  public readonly count: number;
+
+  constructor(count: number) {
+    this.count = count;
+  }
+}
+
+export type SessionLine = SessionShareData | HiddenShares | DeletedSharesCount;
