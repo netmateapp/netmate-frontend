13546d5578526cf0650b6f44d21acd4200f02d47
amata1219 <suzukiamata@gmail.com>
2024-06-25 23:16:15 +0900

    追加: 共有の削除ログ

---
+++ src/routes/sessions/[id=uuidv7]/session/DeletedSharesLog.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { _ } from "./session.svelte";
+
+  type Props = {
+    deletedSharesCount: number;
+  };
+
+  let { deletedSharesCount }: Props = $props();
+</script>
+
+<div class="log">
+  <span class="label">{_("deleted_shares_log", { sharesCount: deletedSharesCount })}</span>
+</div>
+
+<style>
+  .log {
+    display: flex;
+    width: 63.125rem;
+    padding: 0.5rem 1rem;
+    align-items: flex-start;
+    gap: 1rem;
+  }
+
+  .label {
+    color: var(--light-gray);
+    font-family: var(--primary-font);
+    font-size: 0.9375rem;
+  }
+</style>
